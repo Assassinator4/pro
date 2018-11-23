aa23cd2bdc784ec3265e4a1fea6e99525b8104e2
DB-Chen <1070284206@qq.com>
2018-11-23 09:40:59 +0800

    添加了评价左右点击事件

--- js/evaluate.js
+++ js/evaluate.js
@@ -1,12 +1,29 @@
 $(function () {
 
-
+    var index = 0;
     $.each($(".ga_wrap").children("a"),function () {
         $(this).on("click",function () {
+            index = $(this).index();
+            console.log(index);
             $(".panel").css({"display":"block"});
+            $(".panel .panel-main").children().attr("src","images/evaluate/"+index+".jpg");
         })
     });
-    $(".v-click").on("click",function (e) {
+    $(".v-click").on("click",function () {
         $(".panel").css({"display":"none"});
+    });
+    
+    $(".v-left").on("click",function () {
+        index --;
+        if (index > 100) index = 1;
+        else if(index < 1) index = 100;
+        $(".panel .panel-main").children().attr("src","images/evaluate/"+index+".jpg");
+    });
+    $(".v-right").on("click",function () {
+        index ++;
+        if (index > 100) index = 1;
+        else if(index < 1) index = 100;
+        $(".panel .panel-main").children().attr("src","images/evaluate/"+index+".jpg");
     })
+    
 });
